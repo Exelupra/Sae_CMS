ac201cbed765a7b5946456de21f9a62f505731f4
romain0a <romain.auburtin1@etu.univ-lorraine.fr>
2023-06-14 16:48:17 +0200

    ajout du eventListener et de la fonction qui feront que lorsqu'une categorie est cliquée les article de la categorie sont afficher

--- web/js/ListeArcticle.js
+++ web/js/ListeArcticle.js
@@ -5,6 +5,17 @@ function listePlusRecent(){
     afficherArticles(load("/api/articles"));
 }
 
+function listeDeLaCategorie(id){
+    afficherCategories(load("/api/categories/" + id + "/articles"));
+}
+
 function categories(){
     afficherCategories(load("/api/categories"));
 }
+
+document.addEventListener('DOMContentLoaded',function () {
+    listePlusRecent();
+    categories();
+});
+
+document.getElementById("categorie").addEventListener("click",listeDeLaCategorie);
