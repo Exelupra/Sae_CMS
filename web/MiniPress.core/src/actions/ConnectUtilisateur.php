821d6c480cf74ea70ea73da8184201c6605b3068
Exelupra <101408547+Exelupra@users.noreply.github.com>
2023-06-16 11:33:00 +0200

    Enorme commit pour l'utilisateur

--- web/MiniPress.core/src/actions/ConnectUtilisateur.php
+++ web/MiniPress.core/src/actions/ConnectUtilisateur.php
@@ -7,19 +7,16 @@ use http\Client\Response;
 use minipress\core\services\CsrfService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
-use minipress\core\services\UtilisateurService;
 
-class CreationUtilisateurAction extends AbstractAction{
+class ConnectUtilisateur extends AbstractAction{
 
     public function __invoke(Request|ServerRequestInterface $request, Response|ResponseInterface $response, array $args): ResponseInterface
     {
         $data = ['csrf' => CsrfService::generate()];
-        $utilisateur = UtilisateurService::getUtilisateur();
 
         $twix = Twig::fromRequest($request);
         $mergedData = array_merge($data, $this->getGlobalTemplateVar($request));
-        return $twix->render($response, 'inscription.twig', array_merge($mergedData));
+        return $twix->render($response, "authentification.twig", array_merge($mergedData));
     }
 }
