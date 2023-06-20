5d7830b727571785c61e3dfdffdb9c38c27a6d98
romain0a <romain.auburtin1@etu.univ-lorraine.fr>
2023-06-20 11:11:22 +0200

    Mise a jour des lien vers l'API

--- web/MiniPress.web/src/js/Fetcher.js
+++ web/MiniPress.web/src/js/Fetcher.js
@@ -1,5 +1,5 @@
 export function load(element){
-    return fetch('http://localhost/Sae_CMS/web/MiniPress.core/index.php/api' + element).then(response => {
+    return fetch('http://docketu.iutnc.univ-lorraine.fr:27002/Sae_CMS/web/MiniPress.core/index.php/api' + element).then(response => {
         if (response.ok) return response.json();
         return Promise.reject(new Error(response.statusText))
     }).catch(error => console.log(error));
