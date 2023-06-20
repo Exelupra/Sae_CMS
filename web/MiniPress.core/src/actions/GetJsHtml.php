1f185ecf1898d03ff04389c5a0270979bf24aa5d
romain0a <romain.auburtin1@etu.univ-lorraine.fr>
2023-06-20 15:20:02 +0200

    Changement des lien vers le Js

--- web/MiniPress.core/src/actions/GetJsHtml.php
+++ web/MiniPress.core/src/actions/GetJsHtml.php
@@ -10,13 +10,14 @@ class GetJsHtml extends AbstractAction
 
     public function __invoke(Request $request, Response $response, array $args): Response
     {
-        $filePath = __DIR__ . '/../../../web/ListeArticle.html';
+        $filePath = __DIR__ . '/../../../web/' . $args['file'];
+
         if (file_exists($filePath)) {
             $content = file_get_contents($filePath);
             $response->getBody()->write($content);
             return $response;
         } else {
-            $response->getBody()->write('Fichier HTML introuvable');
+            $response->getBody()->write('Fichier introuvable');
             return $response->withStatus(404);
         }
     }
