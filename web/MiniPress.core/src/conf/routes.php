9a318f7eb26bae53fe03a9501fa6ff07f8fa6e00
romain0a <romain.auburtin1@etu.univ-lorraine.fr>
2023-06-21 09:53:28 +0200

    correction du mauvait merge de route

--- web/MiniPress.core/src/conf/routes.php
+++ web/MiniPress.core/src/conf/routes.php
@@ -9,88 +9,78 @@ use minipress\core\api\GetArticlesById;
 use minipress\core\api\GetCategorie;
 use minipress\core\api\GetCategorieById;
 use Slim\Views\Twig;
-    use Psr\Http\Message\ResponseInterface as Response;
-    use Psr\Http\Message\ServerRequestInterface as Request;
-    use \minipress\core\actions\MakeArticleAction;
-    use \minipress\core\actions\MakeArticleProcessAction;
-    use \minipress\core\actions\getAllArticlesAction;
-    use \minipress\core\actions\MakeCategorieAction;
-    use \minipress\core\actions\MakeCategorieProcessAction;
-    use \minipress\core\actions\CreationUtilisateurAction;
-    use \minipress\core\actions\CreationUtilisateurProcess;
-    use \minipress\core\actions\ConnectUtilisateurProcess;
-    use \minipress\core\actions\getUnpublishedArticlesAction;
-    use \minipress\core\actions\PublishArticleAction;
-    use \minipress\core\actions\PublishArticleProcessAction;
-    use \minipress\core\actions\GetArticleByIdAction;
-    use \minipress\core\actions\GetArticleByUserIdAction;
-    use \minipress\core\actions\getMyArticlesAction;
-    use \minipress\core\actions\TogglePublish;
-    use \minipress\core\actions\DeleteArticle;
-    use \minipress\core\actions\DeconnectUtilisateurProcess;
-    use \minipress\core\actions\getUnpublishedArticlesProcessAction;
-    use \minipress\core\actions\GetJsHtml;
-    use \minipress\core\actions\GetJs;
-    use \minipress\core\services\UtilisateurService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use \minipress\core\actions\MakeArticleAction;
+use \minipress\core\actions\MakeArticleProcessAction;
+use \minipress\core\actions\getAllArticlesAction;
+use \minipress\core\actions\MakeCategorieAction;
+use \minipress\core\actions\MakeCategorieProcessAction;
+use \minipress\core\actions\CreationUtilisateurAction;
+use \minipress\core\actions\CreationUtilisateurProcess;
+use \minipress\core\actions\ConnectUtilisateurProcess;
+use \minipress\core\actions\getUnpublishedArticlesAction;
+use \minipress\core\actions\PublishArticleAction;
+use \minipress\core\actions\PublishArticleProcessAction;
+use \minipress\core\actions\GetArticleByIdAction;
+use \minipress\core\actions\GetArticleByUserIdAction;
+use \minipress\core\actions\getMyArticlesAction;
+use \minipress\core\actions\TogglePublish;
+use \minipress\core\actions\DeleteArticle;
+use \minipress\core\actions\GetJs;
+use \minipress\core\actions\GetJsHtml;
 
 
-    return function($app) {
 
-        $app->get('[/]',function (Request $request, Response $response, $args) {
+return function($app) {
 
-            $user = json_decode($_SESSION['user']);
-            $user = json_decode($_SESSION['user']);
-            $twig = Twig::fromRequest($request);
-            if($user != null){
-                return $twig->render($response, 'home.twig', ['user' => $user]);
-            }
-            return $twig->render($response, 'home.twig');
-        })->setName('home');
+    $app->get('[/]',function (Request $request, Response $response, $args) {
 
-        $app->get('/article', getAllArticlesAction::class)->setName('article');
-        $app->get('/article/{id:\d+}[/]', GetArticleByIdAction::class)->setName('articleById');
+        $user = json_decode($_SESSION['user']);
+        $twig = Twig::fromRequest($request);
+        if($user != null){
+            return $twig->render($response, 'home.twig', ['user' => $user]);
+        }
+        return $twig->render($response, 'home.twig');
+    })->setName('home');
 
-        $app->get('/myarticles[/]', getMyArticlesAction::class)->setName('articleByUser');
-        $app->post('/myarticles[/]', TogglePublish::class)->setName('togglePublish');
+    $app->get('/article', getAllArticlesAction::class)->setName('article');
+    $app->get('/article/{id:\d+}[/]', GetArticleByIdAction::class)->setName('articleById');
 
-        $app->get('/myarticles/{id:[a-zA-Z0-9]+}[/]', GetArticleByUserIdAction::class)->setName('myArticleById');
-        $app->post('/myarticles/{id:[a-zA-Z0-9]+}[/]', TogglePublish::class)->setName('togglePublishFromId');
-        $app->get('/article/{id:\d+}[/]', GetArticlesByIdAction::class)->setName('articleById');
+    $app->get('/myarticles[/]', getMyArticlesAction::class)->setName('articleByUser');
+    $app->post('/myarticles[/]', TogglePublish::class)->setName('togglePublish');
 
-        $app->get('/article/create[/]', MakeArticleAction::class)->setName('makeArticle');
-        $app->post('/article/create[/]', MakeArticleProcessAction::class)->setName('madeArticle');
+    $app->get('/myarticles/{id:[a-zA-Z0-9]+}[/]', GetArticleByUserIdAction::class)->setName('myArticleById');
+    $app->post('/myarticles/{id:[a-zA-Z0-9]+}[/]', TogglePublish::class)->setName('togglePublishFromId');
 
-        $app->post('/article/{id:[a-zA-Z0-9]+}/delete', DeleteArticle::class)->setName('deleteArticle');
+    $app->get('/article/create[/]', MakeArticleAction::class)->setName('makeArticle');
+    $app->post('/article/create[/]', MakeArticleProcessAction::class)->setName('madeArticle');
 
-        $app->get('/article/unpublished[/]', getUnpublishedArticlesAction::class)->setName('unpublished');
+    $app->post('/article/{id:[a-zA-Z0-9]+}/delete', DeleteArticle::class)->setName('deleteArticle');
 
-        $app->get('/article/{id}/pusblish[/]', PublishArticleAction::class)->setName('publishArticle');
-        $app->post('/article/{id}/pusblish[/]', PublishArticleProcessAction::class)->setName('publishedArticle');
+    $app->get('/article/unpublished[/]', getUnpublishedArticlesAction::class)->setName('unpublished');
 
-        $app->get('/article/unpublished[/]', getUnpublishedArticlesAction::class)->setName('unpublished');
+    $app->get('/article/{id}/pusblish[/]', PublishArticleAction::class)->setName('publishArticle');
+    $app->post('/article/{id}/pusblish[/]', PublishArticleProcessAction::class)->setName('publishedArticle');
 
-        $app->get('/article/{id}/pusblish[/]', PublishArticleAction::class)->setName('publishArticle');
-        $app->post('/article/{id}/pusblish[/]', PublishArticleProcessAction::class)->setName('publishedArticle');
+    $app->get('/categorie/create[/]', MakeCategorieAction::class)->setName('makeCategorie');
+    $app->post('/categorie/create[/]', MakeCategorieProcessAction::class)->setName('madeCategorie');
 
-        $app->get('/categorie/create[/]', MakeCategorieAction::class)->setName('makeCategorie');
-        $app->post('/categorie/create[/]', MakeCategorieProcessAction::class)->setName('madeCategorie');
+    $app->get('/api/categories[/]', GetCategorie::class)->setName('categorie');
+    $app->get('/api/articles[/]', GetArticle::class)->setName('article');
+    $app->get('/api/categories/{id_categ}/articles', GetArticleByCategorie::class)->setName('articleByCategorie');
+    $app->get('/api/articles/{id}', GetArticlesById::class)->setName('articleById');
+    $app->get('/api/auteurs/{id}/articles', GetArticleByAuteur::class)->setName('articleByAuteur');
+    $app->get('/api/categorie/{id}', GetCategorieById::class)->setName('categorieById');
 
-        $app->get('/api/categories[/]', GetCategorie::class)->setName('categorie');
-        $app->get('/api/articles[/]', GetArticle::class)->setName('article');
-        $app->get('/api/categories/{id_categ}/articles', GetArticleByCategorie::class)->setName('articleByCategorie');
-        $app->get('/api/articles/{id}', GetArticlesById::class)->setName('articleById');
-        $app->get('/api/auteurs/{id}/articles', GetArticleByAuteur::class)->setName('articleByAuteur');
-        $app->get('/api/categorie/{id}', GetCategorieById::class)->setName('categorieById');
+    $app->get('/inscription',CreationUtilisateurAction::class)->setName('inscription');
+    $app->post('/inscription',CreationUtilisateurProcess::class)->setName('inscriptionProcess');
 
-        $app->get('/inscription',CreationUtilisateurAction::class)->setName('inscription');
-        $app->post('/inscription',CreationUtilisateurProcess::class)->setName('inscriptionProcess');
+    $app->get('/connexion', ConnectUtilisateur::class)->setName('connexion');
+    $app->post('/connexion', ConnectUtilisateurProcess::class)->setName('connexionProcess');
 
-        $app->get('/connexion', ConnectUtilisateur::class)->setName('connexion');
-        $app->post('/connexion', ConnectUtilisateurProcess::class)->setName('connexionProcess');
+    $app->post('/deconnexion', DisconnectUtilisateurAction::class)->setName('deconnexion');
 
-        $app->get('/deconnexion', DisconnectUtilisateurAction::class)->setName('deconnexion');
-        $app->post('/deconnexion', DisconnectUtilisateurAction::class)->setName('deconnexion');
-
-        $app->get('/js/{path:.+}', GetJs::class)->setName('js');
-        $app->get('/jsHtml', GetJsHtml::class)->setName('js');
-    };
+    $app->get('/js/{path:.+}', GetJs::class)->setName('js');
+    $app->get('/jsHtml', GetJsHtml::class)->setName('js');
+};
