62b313d88ab1730f24b3376a0adeaaf5465cc99e
oudin-clement <clementoudin3@gmail.com>
2023-06-22 10:13:08 +0200

    correction GetAuteurById

--- web/MiniPress.core/src/api/GetAuteurById.php
+++ web/MiniPress.core/src/api/GetAuteurById.php
@@ -3,6 +3,7 @@
 namespace minipress\core\api;
 
 use minipress\core\actions\AbstractAction;
+use minipress\core\models\Utilisateur;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -11,8 +12,8 @@ class GetAuteurById extends AbstractAction
 
     public function __invoke(Request $request, Response $response, array $args): Response
     {
-        $auteur = \minipress\core\models\Auteur::select('pseudo')->where('id', '=', $args['id'])->get();
-        $response->getBody()->write(json_encode($auteur));
+        $utilisateur = \minipress\core\models\Utilisateur::select('pseudo')->where('id', '=', $args['id'])->get();
+        $response->getBody()->write(json_encode($utilisateur));
         return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
     }
 }
